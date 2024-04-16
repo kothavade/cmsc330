4ecef9375eebd30a536b512d9415539602c4d3b5
Ved Kothavade <ved@kothavade.com>
2024-04-15 23:33:19 -0400

    add ppx_deriving dep (p5)

--- flake.nix
+++ flake.nix
@@ -9,43 +9,47 @@
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      overlays = [
-        rust-overlay.overlays.default
-        (final: prev: {
-          rustToolchain = prev.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" "rustfmt" "clippy" "rust-analyzer" ];
-          };
-        })
-      ];
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs;
-            [
-              # OCaml
-              ocaml
-              ocamlformat
-              # Rust
-              rustToolchain
-              # Misc
-              graphviz
-              gradescope-submit
-            ] ++ (with pkgs.ocamlPackages; [
-              ocaml-lsp
-              dune_3
-              utop
-              ounit
-              qcheck
-              re
-            ]);
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    overlays = [
+      rust-overlay.overlays.default
+      (final: prev: {
+        rustToolchain = prev.rust-bin.stable.latest.default.override {
+          extensions = ["rust-src" "rustfmt" "clippy" "rust-analyzer"];
         };
-      });
-    };
+      })
+    ];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems
+      (system: f {pkgs = import nixpkgs {inherit overlays system;};});
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            # OCaml
+            ocaml
+            ocamlformat
+            # Rust
+            rustToolchain
+            # Misc
+            graphviz
+            gradescope-submit
+          ]
+          ++ (with pkgs.ocamlPackages; [
+            ocaml-lsp
+            dune_3
+            utop
+            ounit
+            qcheck
+            re
+            ppx_deriving
+          ]);
+      };
+    });
+  };
 }
